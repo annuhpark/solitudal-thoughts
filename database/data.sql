e6c0129e354b31a6b5003e3ad204f74a0e0f6c70
Anna Park <parkannuh@gmail.com>
2022-06-23 20:16:57 +0000

    Updated data.sql with better examples to show example groups.

--- database/data.sql
+++ database/data.sql
@@ -7,7 +7,7 @@ values ('admin@gmail.com', '$argon2i$v=19$m=4096,t=3,p=1$b3OBGnXlp7VYNEucbPu6QA$
 returning *;
 
 insert into "groups" ("nameOfGroup", "description", "userId")
-values ('Book Suggestions', 'Any book recommendations for improving your mental health? Feel free to leave your comments below.', 1),
+values ('Self-Help Book Suggestions', "I fell in love with the book '101 Essays That Will Change the Way You Think', by Brianna Wiest. I recommend this book to anyone. Feel free to join if you are a book worm that also loves this genre!", 1),
        ('Pilates in OC', 'Hi everyone, this group is strictly for suggestion on best pilates studios in the Orange County area. Or if anyone is down to get together and take classes together! Thanks for dropping by.', 1),
-       ('Tea Lovers', 'Anyone have any tea recommendations? I feel like they have helped me relax, especially during work. Let me know!', 1)
+       ('Tea Lovers', 'There is this sleeping-aid tea I recommend from Moon Juice. It is called Dream Dust.', 1)
 returning *;
